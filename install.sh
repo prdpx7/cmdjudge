1e946d5bded620c1a180da1961556b1f6f2f6478
zuck007 <pradeepchoudhary009@gmail.com>
2016-10-07 15:58:37 +0530

    :100:

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 
 if [ ! -d $HOME/.cmdjudge ]; then
     git clone https://github.com/zuck007/cmdjudge.git ~/.cmdjudge
+    echo 'alias cmdjudge="$HOME/.cmdjudge/cmdjudge.sh"' >> ~/.bashrc
+    echo 'alias cmdjudge="$HOME/.cmdjudge/cmdjudge.sh"' >> ~/.zshr
+
+    echo "cmdjudge Installed successfully !!(its just an alias bruh)"
+else
+    cd ~/.cmdjudge && git pull origin master && cd -
+    echo "Updated cmdjudge!!!"
 fi
 
-echo 'alias cmdjudge="$HOME/.cmdjudge/cmdjudge.sh"' >> ~/.bashrc
-echo 'alias cmdjudge="$HOME/.cmdjudge/cmdjudge.sh"' >> ~/.zshrc
 
-echo "Installation successful"
